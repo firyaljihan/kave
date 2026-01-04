1e212fdab44f0e5509409349978522427a948ab1
firyaljihan <firyal_haura_30rpl@student.smktelkom-mlg.sch.id>
2026-01-04 15:57:10 +0700

    Feature : REST API add Route Login and Events

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\EventApiController;
+use App\Http\Controllers\Api\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/events', [EventApiController::class, 'index']);
+Route::get('/events/{id}', [EventApiController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/events', [EventApiController::class, 'store']);
+    Route::post('/events/{id}', [EventApiController::class, 'update']);
+    Route::match(['put', 'patch'], '/events/{id}', [EventApiController::class, 'update']);
+    Route::delete('/events/{id}', [EventApiController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
 });
